efafa0054627c644cc42d0bdf657926fbd0b718c
Jaap Gorjup <jaap.gorjup@matrixmind.com>
2022-06-10 20:38:46 +0200

    Added AFAS sample postman collection

--- monitor.py
+++ monitor.py
@@ -1,23 +1,48 @@
 import pycurl
+import json
 from io import BytesIO
 
-#default GET
-buffer = BytesIO()
-c = pycurl.Curl()
-c.setopt(pycurl.OPT_CERTINFO, 1)
-c.setopt(c.URL, 'https://google.com')
-c.setopt(c.WRITEDATA, buffer)
-c.perform()
+def get_data(url='https://google.com', 
+                   method='GET', 
+                   headers={}):
+    ''' 
+    @TODO
+    Support also header values optional
+    Set method GET/POST
+    Validate all options
+    '''
+    #default GET : if POSt build payload
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    c.setopt(pycurl.OPT_CERTINFO, 1)
+    c.setopt(c.URL, url)
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    response =  {
+        "STATUS": c.getinfo(pycurl.HTTP_CODE),
+        "NAMELOOKUP_TIME":  c.getinfo(pycurl.NAMELOOKUP_TIME),
+        "APPCONNECT_TIME)":  c.getinfo(pycurl.APPCONNECT_TIME),
+        "CONNECT_TIME":  c.getinfo(pycurl.CONNECT_TIME),
+        "TOTAL_TIME":  c.getinfo(pycurl.TOTAL_TIME),
+        "SPEED_DOWNLOAD":  c.getinfo(pycurl.SPEED_DOWNLOAD),
+#        "CERTIFICATE":  c.getinfo(pycurl.INFO_CERTINFO)
+    }
+    c.close()
+    return response
 
-# ---- See https://curl.se/libcurl/c/curl_easy_getinfo.html
-print( "Name lookup time : ", c.getinfo(pycurl.NAMELOOKUP_TIME) )
-# Time from start until remote host or proxy completed.
-print( "Connect time : ", c.getinfo(pycurl.CONNECT_TIME) )
-# Time from start until SSL/SSH handshake completed.
-print( "App Connect time: ", c.getinfo(pycurl.APPCONNECT_TIME) )
-# Average download speed
-print( "Download speed: ", c.getinfo(pycurl.SPEED_DOWNLOAD ))
-print( "Totaltime : ",c.getinfo(pycurl.TOTAL_TIME) )
-print('-----------------')
-#print("Certificate dump : %s",c.getinfo(pycurl.INFO_CERTINFO))
-c.close()
+# Opening JSON file
+f = open('config/AFAS API.postman_collection.json')
+data = json.load(f)
+
+# Iterate over them
+for item in data['item']:
+    name = item["name"]
+    method = item["request"]["method"]
+    #headers=item["request"]["headers"]
+    #payload=item["payload"]
+    if 'url' in item['request']:
+        if 'raw' in item['request']['url']:
+            url=item["request"]["url"]["raw"]
+            print(get_data(url=url, method=method))
+    else:
+        print("Skipped ", name)
