cb8b3095235a9af6f00e3e22847a7c73bdba1e75
Alexander Kuemmel <akisys@users.noreply.github.com>
2022-06-23 12:54:41 +0200

    ADD: debug support switches

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,11 +3,16 @@
 set -fue
 
 _SLEEP_TIMER_M="${TEST_FREQUENCY_MINUTES:-"0"}"
+_CMD_OPTS=""
+if [ -n "${DEBUG}" ] && [ "${DEBUG}" = "YES" ];
+then
+  _CMD_OPTS="${_CMD_OPTS} -vvvv"
+fi
 
 while true;
 do
   sleep "${_SLEEP_TIMER_M}m" &
-  /runtime/monitor.py
+  /runtime/monitor.py $_CMD_OPTS
   if [ "${_SLEEP_TIMER_M}" -gt 0 ];
   then
     wait
